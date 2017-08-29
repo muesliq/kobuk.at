921833d1ecb47c575746c989f93cc14ea9abfdd8
Helge Fahrnberger <helge@toursprung.com>
2017-08-29 18:39:36 +0200

    Create page-authors.php

---
+++ oldtheme/page-authors.php
@@ -0,0 +1,40 @@
+<?php /**/?><?php
+/*
+Template Name: All Authors-Page
+*/
+?>
+
+
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content">
+
+<?php get_sidebar('page-top') ?>
+
+<?php the_post() ?>
+			<div id="post-<?php the_ID(); ?>" class="<?php thematic_post_class() ?>">
+    			<?php thematic_postheader(); ?>
+				<div class="entry-content">
+<?php the_content() ?>
+
+<p><ul>
+<?php wp_list_authors('show_fullname=1&optioncount=1&hide_empty=1'); ?>
+</ul></p>
+
+<?php wp_link_pages("\t\t\t\t\t<div class='page-link'>".__('Pages: ', 'thematic'), "</div>\n", 'number'); ?>
+
+<?php edit_post_link(__('Edit', 'thematic'),'<span class="edit-link">','</span>') ?>
+
+				</div>
+			</div><!-- .post -->
+
+<?php if ( get_post_custom_values('comments') ) thematic_comments_template() // Add a key+value of "comments" to enable comments on this page ?>
+
+<?php get_sidebar('page-bottom') ?>
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+
+<?php thematic_sidebar() ?>
+<?php get_footer() ?>
